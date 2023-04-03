265a6716fbdd614771c2433f27557cd681f4762f
harshs0891 <harshs0891@gmail.com>
2023-04-03 18:29:48 +0530

    commiting my first react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+
+// import {
+//   BrowserRouter as Router,
+//   // Switch,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 function App() {
+  const [mode,setMode] = useState('light'); //whether dark mode is enabled or not
+  // const [mode1,setMode1] = useState('light'); //whether dark mode is enabled or not
+  const [modeText,setModeText] = useState("Enable Dark Mode");
+  const [greenText,setgreenText] = useState("Enable Green mode")
+  const [themetext,setThemeText] = useState("Enable purple Mode");
+  const [alert,setAlert] = useState({
+    msg : "your actions will be showed here",
+    type : null
+  });
+
+  const showAlert = (message, type) =>{
+    setAlert({
+      msg:message,
+      type : type
+    })
+    setTimeout(() => {
+      setAlert({
+        msg:"Your actions will be showed here",
+        type:null
+      })
+    }, 1500);
+  }
+
+  const toggleMode = () =>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor ='#042743';
+      setModeText("Enable Light Mode");
+      showAlert("dark mode has been enabled","success")
+      document.title = 'TextUtils - Dark Mode'
+      
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor ='white';
+      setModeText("Enable Dark Mode");
+      showAlert("light mode has been enabled","success")
+      document.title = 'TextUtils - Home'
+    }
+  }
+
+  const toggletheme = () =>{
+    if(mode==='purple'){
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      // setThemeText("Disable Pruple dark theme")
+      setThemeText("Enable Purple theme");
+      showAlert("Purple dark mode is disabled");
+    }else{
+      setMode('purple');
+      document.body.style.backgroundColor ='purple';
+      setThemeText("Disable purple theme");
+      showAlert("purple dark mode has been enabled","success")
+    }
+  }
+
+  const togglegreen = () =>{
+    if(mode==='green'){
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      setgreenText("Enable green mode");
+      showAlert("Green mode is disabled");
+    }else{
+      setMode('green');
+      document.body.style.backgroundColor = 'green';
+      setgreenText("Disable green mode");
+      showAlert("Green Mode is enabled")
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  {/* <Router> */}
+
+{/* <Navbar title="textUtils" AboutText="About TextUtils"/> */}
+<Navbar title="textUtils" mode={mode} toggleMode={toggleMode} toggletheme={toggletheme} themetext={themetext} modeText={modeText} greenText={greenText} togglegreen={togglegreen}/>
+<Alert alert={alert}/>
+{/* <Navbar/> */}
+
+  <div className="container my-3">
+  <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
+    {/* <Routes> */}
+          {/* <Route path="/About" element={<About />} /> */}
+          {/* <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>}/> */}
+    {/* </Routes> */}
+{/* <About/> */}
+  </div>
+{/* // </Router> */}
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,134 @@
+import React,{useState} from 'react'
+
+
+export default function TextForm(props) {
+
+    const handleUpClick = () =>{
+        // console.log("UpperCase was clicked" + text);
+        let newText = text.toUpperCase();
+        // setText("You have clicked on handleUpClick");
+        setText(newText);
+        props.showAlert("characters are converted to UpperCase","Success")
+    }
+
+    const handleLowClick = () =>{
+      let newText = text.toLowerCase();
+      setText(newText)
+      props.showAlert("characters are converted to LowerCase","Success")
+    }
+    
+    const handleClearClick = () =>{
+      let newText = '';
+      setText(newText)
+      props.showAlert("Text cleared","Success")
+    }
+
+    const handleRevWorClick = () =>{
+      // const words = text.split(' ');
+      let strArr = text.split(" ");
+      let newtext = strArr.reverse();
+      let joinWords = newtext.join(" ");
+      setText(joinWords);
+      props.showAlert("Words are reversed","Success")
+    }
+    
+    const handleRevCharClick = () =>{
+      // const words = text.split(' ');
+      let strArr = text.split("");
+      let newtext = strArr.reverse();
+      let joinWords = newtext.join("");
+      setText(joinWords);
+      props.showAlert("characters are reversed","Success")
+      props.showAlert("characters are reversed","Success")
+    }
+
+    const handleCapClick = () =>{
+      let myArr = text.split(" ").map(str =>{
+        return str[0].toUpperCase() + str.slice(1)
+      }).join(" ");
+      setText(myArr);
+      props.showAlert("Texts are capitalised","Success")
+      
+    }
+
+    const handlePTextClick = () =>{
+      let colors = ["red","yellow","cyan","blue"];
+      let x = colors[Math.floor(Math.random() * 4)];
+      document.getElementById("myBox").style.color=x;
+      props.showAlert("color of preview text is changed","Success")
+    }
+
+    const handleIncClick = () =>{
+      document.getElementById("myBox").style.fontSize="130%";
+      props.showAlert("Font size is increased","Success")
+    }
+    
+    const handleDectClick = () =>{
+      document.getElementById("myBox").style.fontSize="80%";
+      props.showAlert("Font size is decreased","Success")
+
+    }
+
+    const handleFindClick = () =>{
+      let newText = text.replaceAll(Find,"");
+      setText(newText);
+
+    }
+
+    const handleCopy = () =>{
+      var texts = document.getElementById("myBox")
+      texts.select();
+      navigator.clipboard.writeText(texts.value);
+    }
+
+
+
+
+    
+    const handleOnChange = (event) =>{
+        // console.log("On Change");
+        setText(event.target.value)
+    }
+
+    const handleOnChangeFind = (event) =>{
+      setFind(event.target.value)
+    }
+
+    const[text,setText]=useState('');
+    const[Find,setFind]=useState('Enter to find');
+    // text="New Text" //wrong way to change the text
+    // setText("New Text"); //correct way to change the text
+  return (
+    <>
+    <div className="container">
+        <h1 style={{color : props.mode=== 'light' ? 'black' : 'red'}}>{props.heading} </h1>
+        <div className="mb-3">
+        <textarea value={text} className="form-control" onChange={handleOnChange} style={{backgroundColor : props.mode==='dark' ? 'grey' : 'white' , color: props.mode=== 'light' ? 'black' : 'white'}} id="myBox" rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary mx-2 my-2"  onClick={handleUpClick}>Convert To UpperCase</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleLowClick}>Convert To LowerCase</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear Text</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleRevWorClick}>Reverse Words</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleRevCharClick}>Reverse Characters</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handlePTextClick}>Change Preview TextColor</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleCapClick}>Capitalise</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleIncClick}>Increase Font size </button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleDectClick}>Decrease Font Size</button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleCopy}>Copy Text</button>
+        <h4 style={{color : props.mode=== 'light' ? 'black' : 'white'}}>Find and Delete Word</h4>
+        <textarea className="my-2 mx-2" value={Find} id="" cols="16" onChange={handleOnChangeFind} rows="1"></textarea>
+        <button className="btn btn-primary mx-2" onClick={handleFindClick}>Delete the word</button>
+
+    </div>
+
+    <div className="container my-3" style={{color : props.mode=== 'light' ? 'black' : 'white'}}>
+      <h2>Your summary</h2>
+      <p>{text.split(/[  ]+/).length} words and {text.length} characters </p>
+      <p>{0.008 * text.split(" ").length} minutes to read</p>
+      {/* <p>{text.search({Find})}</p> */}
+      <h2>Preview</h2>
+      <p>{text.length>0?text:"Enter something to preview it here"}</p>
+    </div>
+    </>
+  )
+}
